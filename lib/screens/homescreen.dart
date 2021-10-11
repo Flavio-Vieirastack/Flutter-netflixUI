0ca8e38496b073740e5378f75c6c7b029d3dca3e
Flavio Vieira <flavioextreme1@hotmail.com>
2021-10-11 16:11:58 -0300

    feito o fade do header do app

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,25 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/custom_appBar.dart';
 
-class Homescreen extends StatelessWidget {
-  const Homescreen({Key? key}) : super(key: key);
+class Homescreen extends StatefulWidget {
+  Homescreen({Key? key}) : super(key: key);
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  double _scroolOffset = 0.0;
+
+  ScrollController? _scroolController;
+
+  @override
+  void initState() {
+    _scroolController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scroolOffset = _scroolController!.offset;
+        });
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scroolController!.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: PreferredSize(
-          child: CustomAppbar(),
+          child: CustomAppbar(
+            scroolOffset: _scroolOffset,
+          ),
           preferredSize: Size(
             screenSize.width,
             50.0,
           )),
-      body: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.grey[850] ?? Colors.grey,
-          child: const Icon(Icons.cast),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.grey[850] ?? Colors.grey,
+        child: const Icon(Icons.cast),
+      ),
+      body: CustomScrollView(
+        controller: _scroolController,
+        slivers: [
+          SliverToBoxAdapter(child: Container(height: 1000.0, color: Colors.blue,),)
+        ],
       ),
     );
   }
